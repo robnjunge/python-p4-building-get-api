2df17f219757242c74474922c6df1f74d6957228
robertnjunge <mubearobert@gmail.com>
2023-10-18 21:50:52 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,50 +1,56 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from flask_migrate import Migrate
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+from models import db, User, Review, Game
 
-db = SQLAlchemy(metadata=metadata)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-class Game(db.Model):
-    __tablename__ = 'games'
+migrate = Migrate(app, db)
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=True)
-    genre = db.Column(db.String)
-    platform = db.Column(db.String)
-    price = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+db.init_app(app)
 
-    reviews = db.relationship('Review', backref='game')
+@app.route('/')
+def index():
+    return "Index for Game/Review/User API"
 
-    def __repr__(self):
-        return f'<Game {self.title} for {self.platform}>'
+@app.route('/games')
+def games():
+    # games=[]
+    # for game in Game.query.all():
+    #     user_dict={
+    #         'title':game.title,
+    #         'genre':game.genre,
+    #         "platform": game.platform,
+    #         "price": game.price,
+    #     }
+    #     games.append(user_dict)
 
-class Review(db.Model):
-    __tablename__ = 'reviews'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    score = db.Column(db.Integer)
-    comment = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    games=[game.to_dict() for game in Game.query.all()]
+     
+    response=make_response(jsonify(games),200)
 
-    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    response.headers['Content-Type']='application/json'
+    return response
 
-    def __repr__(self):
-        return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game=Game.query.filter_by(id=id).first()
+    # game_dict={
+    #     'title':game.title,
+    #     'genre':game.genre,
+    #     'platform': game.platform,
+    #     'price':game.price
+    # }
+    game_dict=game.to_dict()
+    response=make_response(jsonify(game_dict),200)
+    response.headers['Content-Type']='application/json'
+    return response
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    reviews = db.relationship('Review', backref='user')
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
